bec9ac5f912a3eefe52c91ac03675de15b1b0362
Rajan Agarwal <rajan.ag005@gmail.com>
2021-09-18 12:21:25 -0400

    add points system

--- src/components/Points.js
+++ src/components/Points.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import "./Component.css";
 import { Link } from 'react-router-dom';
+import CountUp from 'react-countup';
 
 class Points extends Component {
+
+
   render() {
     return (
         <div className="points" style={{fontSize: '20px'}}>
-        Current Points: 39 ∆<br/>
-        <Link to="/dashboard">View Your Profile</Link>
+            <Link to="/dashboard">View Your Profile</Link>
+            <div>
+            <CountUp
+  start={0}
+  end={100000}
+  duration={4000000}
+  separator=" "
+  decimals={1}
+  decimal="."
+  prefix="Points Total: "
+  delay={0}
+  suffix=" ∆"
+  onEnd={() => console.log('Ended! 👏')}
+  onStart={() => console.log('Started! 💨')}
+>
+  {({ countUpRef, start }) => (
+    <div>
+      <span ref={countUpRef} />
+    </div>
+  )}
+</CountUp>
+            </div>
+        
     </div>
     );
   }
